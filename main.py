3e63b6c7550ea220a57ecd127f9c849a801b83b1
GamePad <hejdjdjuduf@gmail.com>
2023-11-04 11:33:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,32 @@ SERVER_ADDRESS_PVE = "212.12.17.122",28015
 SERVER_ADDRESS_RUST = "212.12.17.122",30015
 SERVER_ADDRESS_ZN = "212.12.17.122",31015
 
-client = commands.Bot(command_prefix=PREFIX, intents=intents) 
+client = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+def GetServerInfo(ADDRESS):
+    print(a2s.info(ADDRESS))
+    print(a2s.players(ADDRESS))
+
+    CLR = discord.Color.green()
+
+    emb = discord.Embed(title = a2s.info(ADDRESS).server_name, colour = CLR)
+    emb.add_field(name = "=======================================", value="", inline=False)
+
+    COUNTER = 0
+
+    for I in a2s.players(ADDRESS):
+        emb.add_field(name = "Игрок: " + I.name + " | Счет: " + str(I.score) + " | Время игры: " + str(datetime.timedelta(seconds = math.ceil(I.duration))), value = "",  inline=False)
+        emb.set_footer(text = a2s.info(ADDRESS).game)
+
+        COUNTER += 1
+
+    if COUNTER == 0:
+        emb.add_field(name = "Сервер пуст",value="",inline=False)
+        emb.set_footer(text = a2s.info(ADDRESS).game)
+    
+    emb.add_field(name = "=======================================", value="", inline=False)
+
+    return emb
 
 @client.event
 
@@ -70,83 +95,17 @@ async def перемога(ctx):
 @client.command()
 
 async def пве(ctx):
-    print(a2s.info(SERVER_ADDRESS_PVE))
-    print(a2s.players(SERVER_ADDRESS_PVE))
-
-    CLR = discord.Color.green()
-
-    emb = discord.Embed(title = a2s.info(SERVER_ADDRESS_PVE).server_name, colour = CLR)
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    COUNTER = 0
-
-    for I in a2s.players(SERVER_ADDRESS_PVE):
-        emb.add_field(name = "Игрок: " + I.name + " | Счет: " + str(I.score) + " | Время игры: " + str(datetime.timedelta(seconds = math.ceil(I.duration))), value = "",  inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_PVE).game)
-
-        COUNTER += 1
-
-    if COUNTER == 0:
-        emb.add_field(name = "Сервер пуст",value="",inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_RUST).game)
-    
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    await ctx.reply(embed = emb)
+    await ctx.reply(embed = GetServerInfo(SERVER_ADDRESS_PVE))
 
 @client.command()
 
 async def зн(ctx):
-    print(a2s.info(SERVER_ADDRESS_ZN))
-    print(a2s.players(SERVER_ADDRESS_ZN))
-
-    CLR = discord.Color.green()
-
-    emb = discord.Embed(title = a2s.info(SERVER_ADDRESS_ZN).server_name, colour = CLR)
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    COUNTER = 0
-
-    for I in a2s.players(SERVER_ADDRESS_ZN):
-        emb.add_field(name = "Игрок: " + I.name + " | Счет: " + str(I.score) + " | Время игры: " + str(datetime.timedelta(seconds = math.ceil(I.duration))), value = "",  inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_ZN).game)
-
-        COUNTER += 1
-
-    if COUNTER == 0:
-        emb.add_field(name = "Сервер пуст",value="",inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_RUST).game)
-    
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    await ctx.reply(embed = emb)
+    await ctx.reply(embed = GetServerInfo(SERVER_ADDRESS_ZN))
 
 @client.command()
 
 async def раст(ctx):
-    print(a2s.info(SERVER_ADDRESS_RUST))
-    print(a2s.players(SERVER_ADDRESS_RUST))
-
-    CLR = discord.Color.green()
-
-    emb = discord.Embed(title = a2s.info(SERVER_ADDRESS_RUST).server_name, colour = CLR)
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    COUNTER = 0
-
-    for I in a2s.players(SERVER_ADDRESS_RUST):
-        emb.add_field(name = "Игрок: " + I.name + " | Счет: " + str(I.score) + " | Время игры: " + str(datetime.timedelta(seconds = math.ceil(I.duration))), value = "",  inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_RUST).game)
-
-        COUNTER += 1
-
-    if COUNTER == 0:
-        emb.add_field(name = "Сервер пуст",value="",inline=False)
-        emb.set_footer(text = a2s.info(SERVER_ADDRESS_RUST).game)
-    
-    emb.add_field(name = "=======================================", value="", inline=False)
-
-    await ctx.reply(embed = emb)
+    await ctx.reply(embed = GetServerInfo(SERVER_ADDRESS_RUST))
 
 token = open("token.txt", "r").readline()
 client.run(token)
